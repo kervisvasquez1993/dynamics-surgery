5bf04f3a4fbfa1d0e5d75500943e679df962f749
kervis vasquez <kervisvasquez24@gmail.com>
2020-04-01 08:33:27 -0700

    agregamos las hojas de stylos

--- functions.php
+++ functions.php
@@ -1,18 +1,14 @@
 <?php 
 // Cargar los estilos y los script de la pagina
+function style(){
 
-
-    function style(){
-     
-        wp_enqueue_style('materialize','https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css', array(),'1.0.0'.'all');
-       // wp_enqueue_style('fontawesome',get_template_directory_uri().'/css/font-awesome.css',array(),'4','all');
-        wp_enqueue_style('style',get_stylesheet_uri());
-        //wp_enqueue_script('sc6',get_template_directory_uri().'/js/es6.js',array(),'1.0.0',true);
-
-        
-        wp_enqueue_script('materialize-js','https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.jshttps://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js', array('jquery'),'4.2.3',true);
-        wp_enqueue_script('scripts', get_template_directory_uri().'/js/scripts.js',array(),'1.0.0', true);
-
+    wp_enqueue_style('materialize','https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css', array(),'1.0.0'.'all');
+    // wp_enqueue_style('fontawesome',get_template_directory_uri().'/css/font-awesome.css',array(),'4','all');
+    wp_enqueue_style('style',get_stylesheet_uri());
+    // cargar los script de materialize y scripts
+    wp_enqueue_script('materialize-js','https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js', array(),'1.0.0', true);
+    wp_enqueue_script('scripts', get_template_directory_uri().'/js/scripts.js',array(),'1.0.0', true);
+    
     }
-
+    
     add_action('wp_enqueue_scripts','style');
--- index.php
+++ index.php
@@ -1,1 +1,27 @@
-<h1>hola</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php wp_head();?>
+</head>
+<body>
+<div class="row">
+      <div class="col s1">1</div>
+      <div class="col s1">2</div>
+      <div class="col s1">3</div>
+      <div class="col s1">4</div>
+      <div class="col s1">5</div>
+      <div class="col s1">6</div>
+      <div class="col s1">7</div>
+      <div class="col s1">8</div>
+      <div class="col s1">9</div>
+      <div class="col s1">10</div>
+      <div class="col s1">11</div>
+      <div class="col s1">12</div>
+    </div>
+ 
+    <?php wp_footer()?>
+</body>
+</html>
