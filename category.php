fb67ec2b83ab2c217837a93799d3257f1b541979
kervisvasquez1993 <kervisvasquez24@gmail.com>
2020-04-27 11:03:45 -0400

    agregamos seccion de menu de categorias

--- category.php
+++ category.php
@@ -1,10 +1,30 @@
 <?php get_header() ;
+// variables para obtener categorias
    $list_subcat = array();
-   $cat;
+   $cat; 
    $the_query = new WP_Query( array( 'cat' => $cat, 'posts_per_page' => 10 ) );
    $parent_cat = get_category_link($cat);
-   $cat2 = $the_query->query[cat];
-   
+   $cat2 = $the_query->query[cat]; //id de la categoria    
+  // echo get_cat_name($cat2); // nombre de la categoria 
+//fin de variables
+
+ $categories = get_categories(array('orderby' => 'name','order'   => 'ASC')); //traer las categorias 
+
+foreach( $categories as $category ) {
+   $category_link = sprintf( 
+       '<a href="%1$s" alt="%2$s">%3$s</a>',
+       esc_url( get_category_link( $category->term_id ) ),
+       esc_attr( sprintf( __( 'View all posts in %s', 'textdomain' ), $category->name ) ),
+       esc_html( $category->name )
+   );
+    
+   echo '<p>' . sprintf($category_link ) . '</p> ';
+  
+} 
+
+ 
+
+
 ?>
 
 
--- functions.php
+++ functions.php
@@ -60,3 +60,4 @@ function widgets_sidebar(){
     ));
 }
 
+
